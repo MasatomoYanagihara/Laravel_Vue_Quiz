22f29fe790e4238a351921f67b393380688935f5
Yana <masatomo.yanagihara.jp@gmail.com>
2020-09-16 17:32:15 +0900

    11-2 Laravel-Adminのカスタマイズ（お知らせ・カテゴリー・一般ユーザー管理の実装）

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -10,7 +10,15 @@ Route::group([
     'middleware'    => config('admin.route.middleware'),
     'as'            => config('admin.route.prefix') . '.',
 ], function (Router $router) {
-
     $router->get('/', 'HomeController@index')->name('home');
+
+    // 管理画面のURL+/informationにリソースコントローラーを割り当て
+    $router->resource('/information', InformationController::class);
+
+    // 管理画面のURL+/categoriesにリソースコントローラーを割り当て
+    $router->resource('/categories', CategoryController::class);
+
+    // 管理画面のURL+/usersにリソースコントローラーを割り当て
+    $router->resource('/users', UserController::class);
 
 });
